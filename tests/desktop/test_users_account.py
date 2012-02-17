a039dbd3dc3fc3216a540167fce00e3b103c7f92
bebef1987 <florin.strugariu@softvision.ro>
2012-02-17 15:57:48 +0200

    login/logout test

---
+++ tests/desktop/test_users_account.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import pytest
+from unittestzero import Assert
+
+from pages.desktop.home import Home
+
+
+class TestAccounts:
+
+    @pytest.mark.nondestructive
+    def test_user_can_login_and_logout_using_browser_id(self, mozwebqa):
+        """ Test for litmus 7857
+        https://litmus.mozilla.org/show_test.cgi?id=7857
+        Test for litmus 4859
+        https://litmus.mozilla.org/show_test.cgi?id=4859
+        """
+
+        home_page = Home(mozwebqa)
+        home_page.go_to_homepage()
+        home_page.login()
+
+        Assert.true(home_page.is_the_current_page)
+        Assert.true(home_page.header.is_user_logged_in)
+
+        home_page.header.click_logout()
+        Assert.false(home_page.header.is_user_logged_in)
