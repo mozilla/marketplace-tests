d332381c348512ab3b3dec4ee536ec0034fdce97
bebef1987 <bebef_1987@yahoo.com>
2012-05-23 00:39:57 +0300

    change method to avoid asserting before page load

--- pages/desktop/consumer_pages/base.py
+++ pages/desktop/consumer_pages/base.py
@@ -14,7 +14,7 @@ class Base(Page):
     _loading_balloon_locator = (By.CSS_SELECTOR, '#site-header > div.loading.balloon.active')
 
     def wait_for_ajax_on_page_finish(self):
-        WebDriverWait(self.selenium, self.timeout).until(lambda s: not self.is_element_present(*self._loading_balloon_locator))
+        WebDriverWait(self.selenium, self.timeout).until(lambda s: not self.is_element_visible(*self._loading_balloon_locator))
 
     def login(self, user="default"):
         from pages.desktop.login import Login
