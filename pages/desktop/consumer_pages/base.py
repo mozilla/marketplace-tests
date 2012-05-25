3256ca6b7d007ea4fdd16c7c2088d10214fac5a2
Teodosia <teodosia.pop@gmail.com>
2012-05-25 13:55:23 +0300

    Updated _logout_locator and corrected typo.

--- pages/desktop/consumer_pages/base.py
+++ pages/desktop/consumer_pages/base.py
@@ -34,7 +34,7 @@ class Base(Page):
     class FooterRegion(Page):
 
         _account_controller_locator = (By.CSS_SELECTOR, "#site-footer > a:nth-child(1)")
-        _logout_locator = (By.CSS_SELECTOR, "#site-footer > nav.footer-links > a:nth-child(1)")
+        _logout_locator = (By.CSS_SELECTOR, "#site-footer > a:nth-child(2)")
         _account_settings_locator = (By.CSS_SELECTOR, "#site-footer > nav.footer-links > a:nth-child(3)")
 
         @property
