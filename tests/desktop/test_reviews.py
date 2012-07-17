16c760f0e1d30f0d0d2a610c5f6d34c78ffe6623
Teodosia <teodosia.pop@gmail.com>
2012-07-17 13:43:57 +0300

    Added test that checks the deletion of a review

--- tests/desktop/test_reviews.py
+++ tests/desktop/test_reviews.py
@@ -47,3 +47,40 @@ class TestReviews:
         Assert.equal(review.rating, rating)
         Assert.equal(review.author, mozwebqa.credentials['default']['name'])
         Assert.equal(review.text, body)
+
+    def test_that_checks_the_deletion_of_a_review(self, mozwebqa):
+        """
+        https://moztrap.mozilla.org/manage/case/648/
+        """
+        test_app = "Test App (whateer1979)"
+        # Step 1 - Login into Marketplace
+        home_page = Home(mozwebqa)
+
+        home_page.go_to_homepage()
+        home_page.login()
+        Assert.true(home_page.is_the_current_page)
+
+        # Step 2 - Search for the test app and go to its details page
+        search_page = home_page.header.search(test_app)
+        details_page = search_page.results[0].click_name()
+        Assert.true(details_page.is_the_current_page)
+
+        Assert.true(details_page.is_submit_review_link_visible)
+        Assert.equal(details_page.submit_review_link, "Submit a Review")
+
+        # Step 3 - Write a review
+        body = 'Automatic app review by Selenium tests %s' % datetime.now()
+        rating = random.randint(1, 5)
+        add_review_page = details_page.click_submit_review()
+        reviews_page = add_review_page.write_a_review(rating, body)
+
+        # Step 4 - Check review
+        Assert.true(reviews_page.is_success_message_visible)
+        Assert.equal(reviews_page.success_message, "Your review was successfully added!")
+
+        # Step 5 - Delete review
+        review = reviews_page.reviews[0]
+        review.delete()
+        Assert.true(reviews_page.is_success_message_visible)
+        Assert.equal(reviews_page.success_message, "Your review was successfully deleted!")
+        Assert.false(review.is_review_visible)
