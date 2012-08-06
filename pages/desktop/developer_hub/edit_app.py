70cf6602e9a098d58a2b0b00f245b3073d030998
Teodosia <teodosia.pop@gmail.com>
2012-08-06 15:49:09 +0300

    Updated locators

--- pages/desktop/developer_hub/edit_app.py
+++ pages/desktop/developer_hub/edit_app.py
@@ -126,11 +126,11 @@ class EditListing(Base):
 
         """
         _name_initial_locator = (By.ID, 'id_name_0')
-        _name_after_failure_locator = (By.ID, 'id_name_2')
+        _name_after_failure_locator = (By.CSS_SELECTOR, '#trans-name .unsaved')
         _url_end_locator = (By.ID, 'id_slug')
         _manifest_url_locator = (By.CSS_SELECTOR, '#manifest-url > td > input')
-        _summary_initial_locator = (By.ID, 'id_summary_0')
-        _summary_after_failure_locator = (By.ID, 'id_summary_2')
+        _summary_initial_locator = (By.CSS_SELECTOR, '#trans-summary [style="display: block;"]')
+        _summary_after_failure_locator = (By.CSS_SELECTOR, '#trans-summary .unsaved')
         _summary_char_count_locator = (By.CSS_SELECTOR, 'div.char-count')
         _categories_locator = (By.CSS_SELECTOR, 'ul.addon-categories > li')
         _device_type_locator = (By.CSS_SELECTOR, '#addon-device-types-edit > ul > li')
