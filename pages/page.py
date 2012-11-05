e21d2f17aace599b900f580ef36842c0be7f8bf5
Florin Strugariu <bebef_1987@yahoo.com>
2012-11-05 12:04:01 +0200

    fix pep8

--- pages/page.py
+++ pages/page.py
@@ -12,15 +12,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from urlparse import urljoin
 
+
 class Page(object):
-    '''
+    """
     Base class for all Pages
-    '''
+    """
 
     def __init__(self, testsetup):
-        '''
+        """
         Constructor
-        '''
+        """
         self.testsetup = testsetup
         self.base_url = testsetup.base_url
         self.selenium = testsetup.selenium
@@ -28,9 +29,9 @@ class Page(object):
         self._selenium_root = hasattr(self, '_root_element') and self._root_element or self.selenium
 
     def generate_url(self, url_path):
-        '''
+        """
         generate the url from the base url adding the url_path to it
-        '''
+        """
         return urljoin(base=self.base_url, url=url_path)
 
     def get_url(self, url):
@@ -131,4 +132,3 @@ class PageRegion(Page):
     def __init__(self, testsetup, element):
         self._root_element = element
         Page.__init__(self, testsetup)
-
