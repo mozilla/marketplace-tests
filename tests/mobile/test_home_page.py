c65fd9d311e2f4459cafdba449465dc3faea9590
Teodosia <teodosia.pop@gmail.com>
2012-12-05 12:57:47 +0200

    [mobile]Removeed test_that_verifies_featured_application_section from the mobile suite. The test is no longer needed.

--- tests/mobile/test_home_page.py
+++ tests/mobile/test_home_page.py
@@ -13,16 +13,6 @@ from pages.mobile.home import Home
 class TestHomepage():
 
     @pytest.mark.nondestructive
-    def test_that_verifies_featured_application_section(self, mozwebqa):
-
-        home_page = Home(mozwebqa)
-        home_page.go_to_homepage()
-
-        # Check if featured section is visible and contains applications
-        Assert.true(home_page.is_featured_section_visible)
-        Assert.equal(home_page.featured_section_elements_count, 6)
-
-    @pytest.mark.nondestructive
     def test_that_verifies_categories_section(self, mozwebqa):
 
         home_page = Home(mozwebqa)
