cb914406f0c84c14545006da4d2d586d9fe4e8e9
Bebe <bebe@P5171.(none)>
2013-01-07 19:24:28 +0200

    update

--- mocks/marketplace_api.py
+++ mocks/marketplace_api.py
@@ -8,7 +8,7 @@ from unittestzero import Assert
 from marketplace import Client
 import json
 
-MARKETPLACE_DOMAIN = 'marketplace-dev.allizom.org'
+DEFAULT_DOMAIN = 'marketplace-dev.allizom.org'
 
 
 class MarketplaceAPI:
@@ -16,7 +16,7 @@ class MarketplaceAPI:
     def __init__(self, credentials=None, domain=None):
         consumer_key = credentials and credentials["key"] or None
         consumer_secret = credentials and credentials["secret"] or None
-        domain= domain or MARKETPLACE_DOMAIN
+        domain= domain or DEFAULT_DOMAIN
         self._client = Client(
             domain=domain,
             consumer_key=consumer_key,
@@ -24,8 +24,7 @@ class MarketplaceAPI:
 
     def _validate_manifest(self, mock_app_url):
         response = self._client.validate_manifest(mock_app_url)
-        self.manifest_id = json.loads(response.content)['id']
-        return self.manifest_id
+        return json.loads(response.content)['id']
 
     def submit_app(self, mock_app):
         # Get the manifest validation result id
@@ -34,46 +33,41 @@ class MarketplaceAPI:
         validation_report = self._client.is_manifest_valid(manifest_validation_id)
         Assert.true(validation_report, "The manifest url is not valid.\n Validation report:\n %s" %validation_report)
 
-        response = self._client.create(self.manifest_id)
+        response = self._client.create(manifest_validation_id)
 
         Assert.equal(response.status_code, 201, "Invalid status code.\n Status code received is: %s" %response.status_code)
-        mock_app["app_id"] = json.loads(response.content)['id']
+        mock_app['id'] = json.loads(response.content)['id']
 
     def update_app_data(self, mock_app):
         # update the default app data with the custom mock app information
 
         data = {
-            'name': mock_app['name'],
-            'summary': mock_app['summary'],
+            'name': mock_app.name,
+            'summary': mock_app.summary,
             'categories': [],
-            'support_email': mock_app['support_email'],
+            'support_email': mock_app.support_email,
             'device_types': [],
-            'payment_type': mock_app['payment_type'],
+            'payment_type': mock_app.payment_type,
             'premium_type': 'free',
-            'privacy_policy': mock_app['privacy_policy'],
-
-            'description': mock_app['description'],
-            'homepage': mock_app['homepage'],
-            'support_url': mock_app['support_website']
+            'privacy_policy': mock_app.privacy_policy,
+            'description': mock_app.description,
+            'homepage': mock_app.homepage,
+            'support_url': mock_app.support_website
         }
 
-        for device in mock_app['device_type']:
-            if device[1]:
-                data['device_types'].append(device[0])
+        # device_types: a list of the device types at least one of: 'desktop', 'android-tablet', 'android-mobile', 'firefoxos'
+        data['device_types'] = [device[0] for device in mock_app['device_type'] if device[1]]
 
-        available_categories = self.get_categories
-        for available_category in available_categories:
-            for required_category in mock_app['categories']:
-                if available_category['name'] == required_category[0]:
-                    data['categories'].append(available_category['id'])
+        Assert.false(not data['device_types'], 'insufficient data added device_types')
 
-        if data['device_types'] is []:
-            return 'insufficient data added device_types == %s' % data['device_types']
+        # categories: a list of the categories, at least two of the category ids provided from the category api
+        data['categories'] = [category['id'] for category in self._categories
+                                if category['name'] in [mock_category[0] for mock_category in mock_app.categories]]
 
-        if len(data['categories']) < 2:
-            return 'insufficient data added categories == %s' % data['categories']
+        Assert.greater_equal(len(data['categories']), 2,
+            'Insufficient data added categories == %s\n Minimum 2 categories required' % data['categories'])
 
-        response = self._client.update(mock_app["app_id"], data)
+        response = self._client.update(mock_app.id, data)
 
         Assert.equal(response.status_code, 202, "Update app data failed.\n Status code %s" %response.status_code)
 
@@ -81,23 +75,22 @@ class MarketplaceAPI:
         self.add_screenshot(mock_app)
 
     def add_screenshot(self, mock_app):
-        response = self._client.create_screenshot(app_id=mock_app["app_id"], filename=mock_app["screenshot_link"], position=1)
+        response = self._client.create_screenshot(app_id=mock_app.id, filename=mock_app["screenshot_link"], position=1)
         Assert.equal(response.status_code, 201,
             "Screenshot not valid.\n Status code %s\nResponse data %s" %(response.status_code, json.loads(response.content)))
 
     @property
-    def get_categories(self):
-        response = json.loads(self._client.get_categories().content)['objects']
-        return response
+    def _categories(self):
+        return json.loads(self._client.get_categories().content)['objects']
 
     def delete_app(self, mock_app):
         # Not yet implemented: https://bugzilla.mozilla.org/show_bug.cgi?id=816572
-        response = self._client.delete(mock_app["app_id"])
-        Assert.equal(response.status_code, 204,'Delete app failed')
+        response = self._client.delete(mock_app.id)
+        Assert.equal(response.status_code, 204, 'Delete app failed')
 
     def app_status(self, mock_app):
-        response = self._client.status(mock_app["app_id"])
-        Assert.equal(response.status_code, 200, "App status failed\n Status code: %s" %response.status_code)
+        response = self._client.status(mock_app.id)
+        Assert.equal(response.status_code, 200, "App status failed\n Status code: %s" % response.status_code)
 
         return json.loads(response.content)
 
@@ -105,12 +98,12 @@ class MarketplaceAPI:
     def all_apps(self):
         response = self._client.list_webapps()
 
-        Assert.equal(response.status_code, 200, "Get all apps failed\n Status code: %s" %response.status_code)
+        Assert.equal(response.status_code, 200, "Get all apps failed\n Status code: %s" % response.status_code)
         return json.loads(response.content)['objects']
 
     def change_app_status_to_pending(self, mock_app):
         self.change_app_state(mock_app, state='pending')
 
     def change_app_state(self, mock_app, state):
-        response = self._client.app_state(app_id=mock_app["app_id"], status=state)
-        Assert.equal(response.status_code, 202, "App state change failed\n Status code: %s" %response.status_code)
+        response = self._client.app_state(app_id=mock_app.id, status=state)
+        Assert.equal(response.status_code, 202, "App state change failed\n Status code: %s" % response.status_code)
