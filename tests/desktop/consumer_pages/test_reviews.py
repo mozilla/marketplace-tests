5168a3aea1691890439bd1d71b827edb03cae37c
AlinT <trifandreialin@gmail.com>
2013-05-24 16:59:16 +0300

    Fixed xfail according to the latest changes

--- tests/desktop/consumer_pages/test_reviews.py
+++ tests/desktop/consumer_pages/test_reviews.py
@@ -48,6 +48,7 @@ class TestReviews:
         Assert.equal(details_page.first_review_rating, mock_review['rating'])
         Assert.equal(details_page.first_review_body, mock_review['body'])
 
+    @pytest.mark.skipif("webdriver.__version__ >= '2.32.0'", reason='Issue 5499: is_displyed() returns True, even if the element is not visible to the user')
     def test_that_checks_the_deletion_of_a_review(self, mozwebqa):
         """
         https://moztrap.mozilla.org/manage/case/648/
