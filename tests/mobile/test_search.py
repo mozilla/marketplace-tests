3055a083d75a7966e04f65ecfa8718e2445f40ef
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2013-07-22 14:12:45 -0700

    change app search term because evernote is not on prod anymore

--- tests/mobile/test_search.py
+++ tests/mobile/test_search.py
@@ -12,7 +12,7 @@ from pages.mobile.home import Home
 
 class TestSearch():
 
-    search_term = 'Evernote Web'
+    search_term = 'Twitter'
     search_term_with_no_result = "abcdefghij"
 
     @pytest.mark.nondestructive
