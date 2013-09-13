c03316e71aa077232bc7d04ee8077c8e2161abb9
Teodosia Pop <teodosia.pop@gmail.com>
2013-09-13 12:02:58 +0300

    [mobile]Unxfail test_reviews_section

--- tests/mobile/test_details_page.py
+++ tests/mobile/test_details_page.py
@@ -23,11 +23,11 @@ class TestDetails():
         # search
         search_page = home_page.search_for(self.APP_NAME)
 
-        #click first app and load its Details Page
+        # click first app and load its Details Page
         details_page = search_page.results[0].click_app()
         details_page.click_more_button()
 
-        #The verifications required by the testcase
+        # The verifications required by the testcase
         Assert.true(details_page.header.is_back_button_visible)
         Assert.true(self.APP_NAME in details_page.title)
         Assert.true(details_page.is_author_visible)
@@ -37,17 +37,16 @@ class TestDetails():
         Assert.true(details_page.is_description_visible)
 
     @pytest.mark.nondestructive
-    @pytest.mark.xfail(reason="Bug 912838 - Screenshots and app icons are missing from homepage/detail pages")
     def test_reviews_section(self, mozwebqa):
         """https://moztrap.mozilla.org/runtests/run/243/env/112/ - Verify details page for an app - Reviews section"""
         home_page = Home(mozwebqa)
         home_page.go_to_homepage()
 
-        #click first app and load its Details Page
+        # click first app and load its Details Page
         details_page = home_page.featured_apps[0].click()
 
-        #This takes the number of reviews on the details page and based on that number it treats 3 different scenarios:
-        #when the app has reviews, when it has 1 review and when the app isn't rated.
+        # This takes the number of reviews on the details page and based on that number it treats 3 different scenarios:
+        # when the app has reviews, when it has 1 review and when the app isn't rated.
         if details_page.is_app_rated:
             if details_page.reviews_count >= 2:
                 if len(details_page.reviews) == 2:
