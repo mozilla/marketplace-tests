ea9ac24051c99f3368817e5ef882b0203f1ec92b
Andrei Hutusoru <andreihutusoru@P5069.(none)>
2013-11-12 11:27:25 +0200

    Issue 381

--- tests/mobile/test_users_account.py
+++ tests/mobile/test_users_account.py
@@ -25,6 +25,8 @@ class TestAccounts():
         settings_page.click_logout()
         Assert.true(settings_page.is_sign_in_visible)
 
+    @pytest.mark.xfail("config.getvalue('base_url') == 'https://marketplace.allizom.org/'",
+                       reason="[stage] Issue https://github.com/mozilla/marketplace-tests/issues/381")
     @pytest.mark.nondestructive
     def test_user_can_go_back_from_settings_page(self, mozwebqa):
         """
