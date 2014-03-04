9e9bf02264b237ff19001eb4a7510f8a2fad2749
bitgeeky <mozpankaj1994@gmail.com>
2014-03-05 01:43:25 +0530

    [Android] Test to verify the 'View All' link in the Popular/new section #445

--- tests/mobile/test_home_page.py
+++ tests/mobile/test_home_page.py
@@ -40,3 +40,31 @@ class TestHomepage():
         new_apps = home_page.click_new_category_tab()
         Assert.true(home_page.is_new_category_tab_selected)
         Assert.true(len(new_apps) > 0)
+
+    @pytest.mark.nondestructive
+    def test_view_all_link_in_popular_new_tab(self, mozwebqa):
+        """
+        Test to verify the 'View All' link in the New/Popular section
+        """
+        home_page = Home(mozwebqa)
+        home_page.go_to_homepage()
+
+        Assert.true(home_page.is_popular_category_tab_visible)
+        Assert.true(home_page.is_new_category_tab_visible)
+
+        Assert.true(home_page.is_popular_category_tab_selected)
+        popular_apps_search_page = home_page.click_new_popular_view_all_link()
+        popular_apps_search_results = popular_apps_search_page.results
+        Assert.true(len(popular_apps_search_results) == 25)
+        popular_apps_search_page.scroll_to_last_result_item()
+        Assert.true(popular_apps_search_page.is_more_button_visible)
+
+        home_page.go_to_homepage()
+        new_apps = home_page.click_new_category_tab()
+
+        Assert.true(home_page.is_new_category_tab_selected)
+        new_apps_search_page = home_page.click_new_popular_view_all_link()
+        new_apps_search_results = new_apps_search_page.results
+        Assert.true(len(new_apps_search_results) == 25)
+        new_apps_search_page.scroll_to_last_result_item()
+        Assert.true(new_apps_search_page.is_more_button_visible)
