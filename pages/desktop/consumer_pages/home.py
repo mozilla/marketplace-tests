a1f4bea1d96287b542cb13dfb2c808d411480c54
Andrei Hutusoru <andrei.hutusoru@softvision.ro>
2014-08-25 15:39:35 +0300

    Refactor Desktop tests (Feed has landed on dev and stage)

--- pages/desktop/consumer_pages/home.py
+++ pages/desktop/consumer_pages/home.py
@@ -4,40 +4,40 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
-from pages.page import PageRegion
 from pages.desktop.consumer_pages.base import Base
 
 
 class Home(Base):
 
     _page_title = 'Firefox Marketplace'
-    _featured_section_locator = (By.CSS_SELECTOR, '#featured > ol > li')
-    _category_section_title_locator = (By.CSS_SELECTOR, '.cat-icon')
-    _category_count_locator = (By.CSS_SELECTOR, '.cat-icons.c > li:not(:nth-child(1))')
+    _site_navigation_menu_locator = (By.ID, 'site-nav')
+    _category_menu_locator = (By.CSS_SELECTOR, '.categories .desktop-cat-link')
+    _category_count_locator = (By.CSS_SELECTOR, '.categories li')
     _first_app_locator = (By.CSS_SELECTOR, '#featured > ol > li:first-child > a')
     _gallery_section_locator = (By.CLASS_NAME, 'gallery')
+    _item_locator = (By.CSS_SELECTOR, '.app.mini-app')
+    _selected_tab_locator = (By.CSS_SELECTOR, '.navbar .active')
+    _tabs_locator = (By.CSS_SELECTOR, '.navbar a')
+    _view_all_locator = (By.CLASS_NAME, 'view-all')
+    _first_new_app_name_locator = (By.CSS_SELECTOR, '.app-name:nth-child(1)')
 
     def go_to_homepage(self):
         self.selenium.get(self.base_url)
         self.maximize_window()
-        self.wait_for_element_visible(*self._featured_section_locator)
+        self.wait_for_element_visible(*self._site_navigation_menu_locator)
 
     @property
-    def is_featured_section_visible(self):
-        return self.is_element_visible(*self._featured_section_locator)
+    def category_menu_text(self):
+        return self.selenium.find_element(*self._category_menu_locator).text
 
-    @property
-    def featured_section_elements_count(self):
-        return len(self.selenium.find_elements(*self._featured_section_locator))
-
-    @property
-    def category_section_title_text(self):
-        return self.selenium.find_element(*self._category_section_title_locator).text
-
-    def expand_all_categories_section(self):
-        self.selenium.find_element(*self._category_section_title_locator).click()
+    def hover_over_categories_menu(self):
+        hover_element = self.selenium.find_element(*self._category_menu_locator)
+        ActionChains(self.selenium).\
+        move_to_element(hover_element).\
+        perform()
 
     @property
     def categories(self):
@@ -48,44 +48,24 @@ class Home(Base):
     def category_count(self):
         return len(self.selenium.find_elements(*self._category_count_locator))
 
-    def click_on_first_app(self):
-        self.selenium.find_element(*self._first_app_locator).click()
-        from pages.desktop.consumer_pages.details import Details
-        return Details(self.testsetup)
+    @property
+    def first_new_app_name(self):
+        return self.find_element(*self._first_new_app_name_locator).text
 
     @property
-    def gallery_section(self):
-        section = self.find_element(*self._gallery_section_locator)
-        return self.GallerySection(self.testsetup, section)
+    def apps_are_visible(self):
+        return self.is_element_visible(*self._item_locator)
 
-    class GallerySection(PageRegion):
+    @property
+    def elements_count(self):
+        return len(self.find_elements(*self._item_locator))
 
-        _item_locator = (By.CSS_SELECTOR, 'ol > li')
-        _selected_tab_locator = (By.CLASS_NAME, 'active')
-        _tabs_locator = (By.CSS_SELECTOR, '.tabs > a')
-        _view_all_locator = (By.CLASS_NAME, 'view-all')
-        _first_new_app_name_locator = (By.CSS_SELECTOR, '#gallery .info > h3:nth-child(1)')
+    @property
+    def selected_tab_text(self):
+        return self.find_element(*self._selected_tab_locator).text
 
-        @property
-        def first_app_name(self):
-            return self.find_element(*self._first_new_app_name_locator).text
-
-        @property
-        def is_visible(self):
-            return self.is_element_visible(*self._item_locator)
-
-        @property
-        def elements_count(self):
-            return len(self.find_elements(*self._item_locator))
-
-        @property
-        def selected_tab_text(self):
-            return self.find_element(*self._selected_tab_locator).text
-
-        def click_new_tab(self):
+    def click_new_tab(self):
+        if 'Home'.upper() == self.selected_tab_text:
             self.find_elements(*self._tabs_locator)[1].click()
-
-        def click_view_all(self):
-            self.find_element(*self._view_all_locator).click()
-            from pages.desktop.consumer_pages.search import Search
-            return Search(self.testsetup)
+        else:
+            self.find_elements(*self._tabs_locator)[2].click()
--- tests/desktop/consumer_pages/test_consumers_page.py
+++ tests/desktop/consumer_pages/test_consumers_page.py
@@ -27,88 +27,44 @@ class TestConsumerPage:
         Assert.true(home_page.header.is_sign_in_visible)
 
     @pytest.mark.nondestructive
-    def test_that_verifies_featured_application_section(self, mozwebqa):
+    def test_that_verifies_categories_menu(self, mozwebqa):
 
         home_page = Home(mozwebqa)
         home_page.go_to_homepage()
 
-        Assert.true(home_page.is_the_current_page)
+        Assert.equal(home_page.categories.title, 'Categories'.upper())
 
-        # Check if featured section is visible and contains applications
-        Assert.true(home_page.is_featured_section_visible)
-        Assert.true(home_page.featured_section_elements_count > 0)
-
-    @pytest.mark.nondestructive
-    def test_that_verifies_categories_section(self, mozwebqa):
-
-        home_page = Home(mozwebqa)
-        home_page.go_to_homepage()
-
-        Assert.equal(home_page.categories.title, 'All Categories')
-
-        # Open "All Categories" section
-        home_page.expand_all_categories_section()
+        # Hover over "Categories" menu
+        home_page.hover_over_categories_menu()
         Assert.greater(len(home_page.categories.items), 0)
 
-    @pytest.mark.smoke
     @pytest.mark.nondestructive
-    def test_that_clicking_on_featured_app_loads_details_page(self, mozwebqa):
-
-        home_page = Home(mozwebqa)
-        home_page.go_to_homepage()
-
-        details_page = home_page.click_on_first_app()
-        Assert.true(details_page.is_the_current_page)
-
-    @pytest.mark.nondestructive
-    def test_opening_every_category_page_from_categories_section(self, mozwebqa):
-        """In addition to Pivotal #31913855"""
+    def test_opening_every_category_page_from_categories_menu(self, mozwebqa):
 
         home_page = Home(mozwebqa)
         home_page.go_to_homepage()
 
         for i in range(home_page.category_count):
-            home_page.expand_all_categories_section()
+            home_page.hover_over_categories_menu()
             category_name = home_page.categories.items[i].name
             category_page = home_page.categories.items[i].click_category()
             Assert.equal(category_name, category_page.category_title)
             Assert.true(category_page.is_the_current_page)
 
     @pytest.mark.nondestructive
-    def test_that_verifies_gallery_section_tabs(self, mozwebqa):
+    def test_that_verifies_nav_menu_tabs(self, mozwebqa):
 
         home_page = Home(mozwebqa)
         home_page.go_to_homepage()
 
-        Assert.true('Popular' in home_page.gallery_section.selected_tab_text)
-        Assert.true(home_page.gallery_section.is_visible)
-        Assert.true(home_page.gallery_section.elements_count > 0)
+        Assert.true('Home'.upper() in home_page.selected_tab_text)
 
-        home_page.gallery_section.click_new_tab()
+        home_page.click_new_tab()
+        Assert.true('New'.upper() in home_page.selected_tab_text)
+        Assert.true(home_page.apps_are_visible)
+        Assert.true(home_page.elements_count > 0)
 
-        Assert.true('New' in home_page.gallery_section.selected_tab_text)
-        Assert.true(home_page.gallery_section.is_visible)
-        Assert.true(home_page.gallery_section.elements_count > 0)
-
-    @pytest.mark.nondestructive
-    def test_open_view_all_link_while_popular_tab_selected(self, mozwebqa):
-
-        home_page = Home(mozwebqa)
-        home_page.go_to_homepage()
-        Assert.true('Popular' in home_page.gallery_section.selected_tab_text)
-
-        search_page = home_page.gallery_section.click_view_all()
-        Assert.true(search_page.is_the_current_page)
-        Assert.greater(len(search_page.results), 0)
-
-    @pytest.mark.nondestructive
-    def test_open_view_all_link_while_new_tab_selected(self, mozwebqa):
-
-        home_page = Home(mozwebqa)
-        home_page.go_to_homepage()
-        home_page.gallery_section.click_new_tab()
-        Assert.true('New' in home_page.gallery_section.selected_tab_text)
-
-        search_page = home_page.gallery_section.click_view_all()
-        Assert.true(search_page.is_the_current_page)
-        Assert.greater(len(search_page.results), 0)
+        home_page.click_new_tab()
+        Assert.true('Popular'.upper() in home_page.selected_tab_text)
+        Assert.true(home_page.apps_are_visible)
+        Assert.true(home_page.elements_count > 0)
