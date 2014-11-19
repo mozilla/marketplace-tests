b810a535d67869c0d581bf364690e581fcb809a5
Andrei Hutusoru <andrei.hutusoru@softvision.ro>
2014-11-19 18:16:35 +0200

    Add test to login from My Apps on mobile

--- pages/mobile/account_settings.py
+++ pages/mobile/account_settings.py
@@ -20,7 +20,7 @@ class AccountSettings(Base):
     _login_locator = (By.CSS_SELECTOR, '.only-logged-out a:not(.register)')
 
     _settings_options_locator = (By.CSS_SELECTOR, '.nav-settings li a[href="%s"]')
-    _my_apps_locator = (By.CSS_SELECTOR, '.container.listing')
+    _my_apps_locator = (By.CSS_SELECTOR, '.container.listing.only-logged-in')
 
     def __init__(self, testsetup):
         Base.__init__(self, testsetup)
@@ -49,10 +49,17 @@ class AccountSettings(Base):
         from pages.fxa import FirefoxAccounts
         return FirefoxAccounts(self.testsetup)
 
-    def click_apps(self):
+    def click_my_apps(self, logged_in=True):
         self.selenium.find_element(self._settings_options_locator[0], self._settings_options_locator[1] % ("/purchases")).click()
-        WebDriverWait(self.selenium, self.timeout).until(lambda s: self.is_element_visible(*self._my_apps_locator))
+        if logged_in == True:
+            WebDriverWait(self.selenium, self.timeout).until(lambda s: self.is_element_visible(*self._my_apps_locator))
+        else:
+            return AccountSettings(self.testsetup)
 
     @property
     def is_sign_in_visible(self):
         return self.is_element_visible(*self._login_locator)
+
+    @property
+    def are_my_app_visible(self):
+        return self.is_element_visible(*self._my_apps_locator)
