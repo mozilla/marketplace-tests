fd767ec72e539bc87e850cfc7afa78c19065040c
Andrei Hutusoru <andrei.hutusoru@softvision.ro>
2015-01-26 17:48:46 +0200

    Test for adding a new app version

--- mocks/mock_application.py
+++ mocks/mock_application.py
@@ -54,6 +54,8 @@ class MockApplication(dict):
 
         if self['app_type'] == 'packaged':
             self['app_path'] = self._get_resource_path('packaged_app.zip')
+        elif self['app_type'] == 'new_version':
+            self['app_path'] = self._get_resource_path('new_version_app.zip')
         else:
             self['url'] = 'http://%s.testmanifest.com/manifest.webapp' % current_time
 
