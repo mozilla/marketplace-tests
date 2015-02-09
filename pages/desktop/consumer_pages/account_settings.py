84b0cefc3ed838f3a9e1edeeb96db3c8fff16c43
Andrei Hutusoru <andrei.hutusoru@softvision.ro>
2015-02-09 18:00:15 +0200

    Add Payment test for Desktop
    Conflicts:
    	pages/desktop/paypal/paypal_sandbox.py
    	tests/desktop/consumer_pages/test_users_account.py

--- pages/desktop/consumer_pages/account_settings.py
+++ pages/desktop/consumer_pages/account_settings.py
@@ -27,11 +27,6 @@ class AccountSettings(Base):
         self.set_window_size()
         self.selenium.get(self.base_url + '/settings')
 
-    def click_payment_menu(self):
-        self.selenium.find_element(*self._payment_locator).click()
-        self.wait_for_page_loaded()
-        return Payments(self.testsetup)
-
     @property
     def header_title(self):
         return self.selenium.find_element(*self._header_title_locator).text
@@ -144,44 +139,3 @@ class My_Apps(AccountSettings):
         @property
         def are_screenshots_visible(self):
             return self.find_element(*self._screenshots_locator).is_displayed()
-
-
-class Payments(AccountSettings):
-    """
-    Payment Settings page
-    https://marketplace-dev.allizom.org/en-US/settings/payment
-    """
-
-    _page_title = 'Payment Settings | Firefox Marketplace'
-
-    _set_up_pre_approval_locator = (By.CSS_SELECTOR, '#preapproval > footer > button')
-    _pre_approval_enabled_locator = (By.CSS_SELECTOR, '#preapproval .enabled')
-    _remove_pre_approval_locator = (By.CSS_SELECTOR, '#preapproval > footer > button.delete')
-    _preapproval_success_message_locator = (By.CSS_SELECTOR, 'section.notification-box.full > div.success')
-
-    def go_to_payment(self):
-        self.selenium.get('%s/settings/payment/' % self.base_url)
-
-    @property
-    def is_success_message_visible(self):
-        return self.is_element_visible(*self._preapproval_success_message_locator)
-
-    def click_set_up_pre_approval(self):
-        self.selenium.find_element(*self._set_up_pre_approval_locator).click()
-        from pages.desktop.paypal.paypal_sandbox import PayPalSandbox
-        return PayPalSandbox(self.testsetup)
-
-    @property
-    def is_pre_approval_enabled(self):
-        return self.is_element_visible(*self._pre_approval_enabled_locator)
-
-    @property
-    def pre_approval_enabled(self):
-        return self.selenium.find_element(*self._pre_approval_enabled_locator).text
-
-    def click_remove_pre_approval(self):
-        self.selenium.find_element(*self._remove_pre_approval_locator).click()
-
-    @property
-    def is_remove_pre_approval_button_visible(self):
-        return self.is_element_visible(*self._remove_pre_approval_locator)
