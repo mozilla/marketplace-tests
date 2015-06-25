eef34e4acdfd28be063d371bf905af1133198850
Bob Silverberg <bob.silverberg@gmail.com>
2015-06-25 15:32:28 -0700

    Unxfail test_check_submission_of_an_app_with_XSS_in_its_app_name as the bug has been fixed

--- tests/desktop/developer_hub/test_developer_hub_submit_apps.py
+++ tests/desktop/developer_hub/test_developer_hub_submit_apps.py
@@ -234,7 +234,6 @@ class TestDeveloperHubSubmitApps(BaseTest):
             delete_popup = edit_app.click_delete_app()
             delete_popup.delete_app()
 
-    @pytest.mark.xfail(reason='Bug 1154123 - Screenshot thumbnail does not appear on Edit Listing page')
     @pytest.mark.credentials
     def test_check_submission_of_an_app_with_XSS_in_its_app_name(self, mozwebqa, login):
         if '-dev.allizom' in mozwebqa.base_url:
